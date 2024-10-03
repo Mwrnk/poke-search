3bfe883b1be355f2879fbbc28bb61dfc3cabf175
Mateus Werneck <87713168+Mwrnk@users.noreply.github.com>
2024-10-03 18:55:32 -0300

    Criando o componente para listar os Pokémons,  puxando nome, imagem e tipo e passando para o card

---
+++ src/Components/PokemonList/ListPokemon.jsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+import React from "react";
+import PokemonCard from "./CardPokemon";
+
+const PokemonListContainer = styled.div`
+  overflow-y: scroll; /* Garante que o conteúdo seja rolável */
+  max-height: 100vh; /* Define a altura máxima */
+  border-radius: 5px;
+  margin: 0;
+  padding-top: 20px;
+
+  /* Esconder a scrollbar no navegador */
+  scrollbar-width: none; /* Firefox */
+  -ms-overflow-style: none; /* Internet Explorer 10+ */
+
+  &::-webkit-scrollbar {
+    display: none; /* Webkit browsers (Chrome, Safari, etc.) */
+  }
+`;
+
+const ListaPokemon = ({ pokemons }) => {
+  return (
+    <PokemonListContainer>
+      {pokemons.map((pokemon, index) => (
+        <PokemonCard
+          key={index}
+          name={pokemon.data.name}
+          image={pokemon.data.sprites.front_default}
+          type={pokemon.data.types
+            .map((objTipo) => objTipo.type.name)
+            .join(", ")}
+        />
+      ))}
+    </PokemonListContainer>
+  );
+};
+
+export default ListaPokemon;
