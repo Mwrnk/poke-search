4fd2bb4f157ae9dfc0cbfec8680cb12a59c1e7cb
Mateus Werneck <87713168+Mwrnk@users.noreply.github.com>
2024-10-03 18:56:14 -0300

    Adicionando uma animação para os cards dos Pokémons

--- src/Components/PokemonList/CardPokemon.jsx
+++ src/Components/PokemonList/CardPokemon.jsx
@@ -8,12 +8,15 @@ const PokemonCardContainer = styled.div`
   margin-bottom: 15px;
   background-color: #e0e0e0;
   border-radius: 10px;
+  transition: background-color ease 0.25s, transform ease 0.1s; /* Transições */
   cursor: pointer;
 
   &:hover {
     background-color: #c4c3c3;
   }
 
+  transform: ${({ isPressed }) =>
+    isPressed ? "scale(0.95)" : "scale(1)"}; /* Reduz o tamanho ao clicar */
 `;
 
 const PokemonImage = styled.img`
@@ -34,9 +37,14 @@ const PokemonType = styled.p`
 `;
 
 const PokemonCard = ({ name, image, type }) => {
+  const [isPressed, setIsPressed] = useState(false);
 
   return (
     <PokemonCardContainer
+      isPressed={isPressed}
+      onMouseDown={() => setIsPressed(true)} // Define isPressed como true ao pressionar
+      onMouseUp={() => setIsPressed(false)} // Define isPressed como false ao soltar
+      onMouseLeave={() => setIsPressed(false)} // Garante que a animação volte se o mouse sair enquanto pressionado
     >
       <PokemonImage src={image} alt={name} />
       <div>
