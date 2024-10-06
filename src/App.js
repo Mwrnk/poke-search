da05aa305f76ae57bb883b8c868812688a525699
Tiago Malaquias <tiago.malaquias.souza@gmail.com>
2024-10-05 21:18:16 -0300

    Instalando o react-router-dom para melhor gerenciamento de páginas da aplicação

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Pokesearch } from "./pages/Pokesearch";
 import { Home } from "./pages/Home";
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pokesearch" element={<Pokesearch />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
