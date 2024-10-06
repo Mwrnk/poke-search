c3fecdd440f687c6e21ea4bc554854c4fa3459bd
Tiago Malaquias <tiago.malaquias.souza@gmail.com>
2024-10-06 03:28:43 -0300

    Criando a estilização do header

---
+++ src/Components/Header/styles.jsx
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  background-color: #1c1b1f;
+`;
+
+export const Logo = styled.img`
+  width: 150px;
+  height: 50px;
+  cursor: pointer;
+  margin: 10px;
+  padding: 10px;
+`;
+
+export const Nav = styled.div`
+  display: flex;
+  align-items: center;
+  width: auto;
+  height: auto;
+  cursor: pointer;
+  margin: 10px;
+  padding: 10px;
+`;
+
+export const Text = styled.p`
+  font-family: Roboto Mono, "monospace";
+  font-size: 16px;
+  color: white;
+`;
+
+export const Icon = styled.img`
+  width: 24px;
+  height: 24px;
+`;
